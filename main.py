fabbf45822281c07b0c42a9350d7912a05d1cebf
ap-arnab <arnabpaul128@gmail.com>
2025-08-07 12:45:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+## THE PERFECT GUESS: (19th July, 2025)
+
+import random
+n= random.randint(1, 101)
+print("The Game is starting...")
+print("RULES: Guess a number between 1 and 100❗")
+
+a=-1
+guesses = 0
+try:
+    while(a != n):
+        a=int(input("Guess the number: "))
+        if (a >= 100 or a<= (-1)):
+            print("Invalid input given. Try again!")
+        elif (a < n):
+            print("Higher number please ⬆️")
+        elif (a > n):
+            print("Lower number please ⬇️")
+        guesses+=1
+    print(f"🎉 You have guessed the number \"{n}\" correctly in {guesses} attempts")
+
+except ValueError:
+    print('Are you blind or a stupid? I said "NUMBER"😑!!')
+except Exception as e:
+    print(e)
